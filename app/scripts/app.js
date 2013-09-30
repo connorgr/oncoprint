446e7e2f3c660a5ac09bb313dd3b277cebcdf3a3
connor <connor@cs.brown.edu>
2013-09-30 16:51:55 -0400

    Add CrossFilter and d3 testing code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,19 @@
 /*global define */
-define(['d3'], function (d3) {
+define(['crossfilter', 'd3'], function (crossfilter, d3) {
     'use strict';
 
     d3.select('#test_oncoprint').append('p').text('hello, world.');
 
     d3.json('../data/test_HotNet_4.json', function (data) {
-      console.log(data);
+      var rows = data.table_rows,
+          rowKeys = {},
+          sample = crossfilter(data.samples);
+
+      for (var i = 0; i < rows.length; i++) {
+        rowKeys[rows[i][0]] = i;
+      }
+      console.log(sample);
+      console.log(rowKeys);
     });
 
     return '\'Allo \'Allo!';
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,24 @@
 require.config({
   paths: {
+      crossfilter: '../bower_components/crossfilter/crossfilter',
       d3: '../bower_components/d3/d3',
       jquery: '../bower_components/jquery/jquery'
   },
   shim: {
+    crossfilter: {
+      exports: 'crossfilter'
+    },
     d3: {
       exports: 'd3'
     }
   }
 });
 
-require(['app', 'jquery', 'd3'], function (app, $, d3) {
+require(['app', 'jquery', 'd3', 'crossfilter'], function (app, $, d3, crossfilter) {
     'use strict';
     // use app here
     console.log(app);
     console.log('Running jQuery %s', $().jquery);
     console.log('Running d3 %s', d3.version);
+    console.log('Running CrossFilter %s', crossfilter.version);
 });
