b20d73543c78061389e223d6b8db885777fadb0c
connor <connor@cs.brown.edu>
2013-09-30 16:32:06 -0400

    Fix d3 include issues

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,12 @@
 /*global define */
-define([], function () {
+define(['d3'], function (d3) {
     'use strict';
+
+    d3.select('#test_oncoprint').append('p').text('hello, world.');
+
+    d3.json('../data/test_HotNet_4.json', function (data) {
+      console.log(data);
+    });
 
     return '\'Allo \'Allo!';
 });
